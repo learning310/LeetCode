fa33c0559d46314b0fae933b50310e7aecbb4a02
learning310 <1147781396@qq.com>
2020-02-11 18:24:26 +0800

    check out

---
+++ Top-K-Frequent-Element/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Solution.h"
+
+int main() {
+	vector<int> nums = {4,1,-1,2,-1,2,3};
+	int k = 2;
+	vector<int> res = Solution::topKFrequent(nums,k); /* how to handle the repeated element */
+	for(int num : res){
+		std::cout << num << " ";
+	}
+	std::cout << std::endl; 
+	return 0;
+}
